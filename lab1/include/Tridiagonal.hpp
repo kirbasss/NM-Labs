fcf8d8afd4e8494f10755c89057354e1667c8e42
kirbasss <kirillasz3@gmail.com>
2026-03-28 21:38:20 +0300

    New function for loading

--- lab1/include/Tridiagonal.hpp
+++ lab1/include/Tridiagonal.hpp
@@ -3,6 +3,15 @@
 #include <ostream>
 #include <string>
 
+struct TridiagonalSystem {
+    std::vector<double> a; // нижняя диагональ, размер n-1
+    std::vector<double> b; // главная диагональ, размер n
+    std::vector<double> c; // верхняя диагональ, размер n-1
+    std::vector<double> d; // правая часть, размер n
+};
+
+TridiagonalSystem loadTridiagonalSystemFromFile(const std::string& filename);
+
 std::vector<double> tridiagSolve(const std::vector<double>& a,
                                 const std::vector<double>& b,
                                 const std::vector<double>& c,
