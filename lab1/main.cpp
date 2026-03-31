f59402a6160c87eb913ad6ed8038f55760db24a5
kirbasss <kirillasz3@gmail.com>
2026-03-31 17:23:13 +0300

    Запуск

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,7 +1,15 @@
-#include <LU.hpp>
+#include "LU.hpp"
+#include "Tridiagonal.hpp"
+#include "Iterative.hpp"
+#include "Jacobi.hpp"
+#include "QR.hpp"
 
 int main() {
-    run_1_1("../lab1/input/1.1.txt");
+    run_1_1("input/1.1.txt");
+    run_1_2("input/1.2.txt");
+    run_1_3("input/1.3.txt");
+    run_1_4("input/1.4.txt");
+    run_1_5("input/1.5.txt");
 
     return 0;
 }
